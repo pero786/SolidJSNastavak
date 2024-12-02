a5cdcea2bf5d992f46ccbb7b962598d1ca2d3069
pero786 <sarecp3@gmail.com>
2024-12-02 08:33:57 +0100

    dizajn

--- solidNastavak/src/App.jsx
+++ solidNastavak/src/App.jsx
@@ -1,12 +1,22 @@
 import { Router, Route } from "@solidjs/router";
 import Home from "./pages/home";
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <Router root={Layout}>
       <Route path="/" component={Home}/>
     </Router>
   );
 }
 
-export default App;
+function Layout(props) {
+  return(
+    <>
+    <div class="p-4 flex flex-col gap-4">
+      <div>Zaglavlje</div>
+      {props.children}
+      <div>Podnožje</div>
+    </div>
+    </>
+  );
+}
